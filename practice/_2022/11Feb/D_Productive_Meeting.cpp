1fcbe1496e62cc657da2a6b86e165e72b197c301
code-shoot <franksmithattheshoot@gmail.com>
2022-02-12 09:43:16 +0530

    upsolve every single one

---
+++ practice/_2022/11Feb/D_Productive_Meeting.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+
+#define code_brains                 \
+  ;                                 \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(0);
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double lld;
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+#define endl "\n"
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define debug(x)     \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
+#define present(c, x) (c.find(x) != c.end())
+#define desc greater<int>()
+#define fi first
+#define se second
+#define bg begin()
+#define ed end()
+#define set_bits __builtin_popcountint
+#define vl vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define pi pair<int, int>
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.first);
+  cerr << ",";
+  _print(p.second);
+  cerr << "}";
+}
+template <class T>
+void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+///////////////////////////////////////////////////////////////
+
+signed main() {
+  code_brains;
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (auto &i : arr) cin >> i;
+
+    priority_queue<pi> pq;
+    // priority_queue<pi, vector<pi>, greater<pi>> pq;
+
+    for (int i = 1; i <= n; i++)
+      if (arr[i - 1]) pq.push({arr[i - 1], i});
+
+    vector<pi> ans;
+
+    while (!pq.empty() && pq.size() > 1) {
+      // debug(pq.size());
+      pi t1 = pq.top();
+      t1.first -= 1;
+      pq.pop();
+
+      pi t2 = pq.top();
+      t2.first -= 1;
+      pq.pop();
+
+      ans.push_back({t1.second, t2.second});
+
+      if (t1.first > 0) pq.push(t1);
+      if (t2.first > 0) pq.push(t2);
+    }
+
+    cout << ans.size() << '\n';
+    for (auto i : ans) cout << i.first << " " << i.second << '\n';
+  }
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////
