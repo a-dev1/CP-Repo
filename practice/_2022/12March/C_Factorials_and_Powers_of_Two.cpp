d8439836384adae9ffd9fb7b0e4e139ac690aa01
code-shoot <franksmithattheshoot@gmail.com>
2022-03-12 13:04:27 +0530

    1st

---
+++ practice/_2022/12March/C_Factorials_and_Powers_of_Two.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+
+#define code_brains                 \
+  ;                                 \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(0);
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double lld;
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+#define endl "\n"
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define debug(x)     \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
+#define present(c, x) (c.find(x) != c.end())
+#define desc greater<int>()
+#define fi first
+#define se second
+#define bg begin()
+#define ed end()
+// #define set_bits __builtin_popcountint
+// #define set_bits __builtin_popcountll
+#define vl vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.first);
+  cerr << ",";
+  _print(p.second);
+  cerr << "}";
+}
+template <class T>
+void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+///////////////////////////////////////////////////////////////
+ll power(ll b, int pow) {
+  if (pow == 0) return 1;
+
+  if (pow % 2 == 0)
+    return (power(b, pow / 2) * power(b, pow / 2));
+  else
+    return (power(b, pow / 2) * power(b, pow / 2) * b);
+}
+
+signed main() {
+  code_brains;
+  int t;
+  cin >> t;
+  vector<ll> fact(17, 0);
+  ll temp = 1;
+
+  for (int i = 1; i <= 14; i++) {
+    temp *= i;
+    fact[i] = temp;
+  }
+
+  while (t--) {
+    ll n;
+    cin >> n;
+    ll ans = n;
+
+    for (ll i = 0; i < power(2, 14); i++) {
+      ll temp = 0;
+
+      ll shit = i;
+
+      int j = 1;
+      while (shit > 0) {
+        temp += (shit & 1) * fact[j];
+        j++;
+        shit = shit >> 1;
+      }
+
+      if (temp > n) break;
+
+      ll cnt = __builtin_popcountll(i) + __builtin_popcountll(n - temp);
+      ans = min(ans, cnt);
+    }
+    cout << ans << '\n';
+  }
+
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////
