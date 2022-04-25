54e0b54ce48deae9aa006fab00569c8fc5fe5fe7
code-shoot <franksmithattheshoot@gmail.com>
2022-04-26 01:03:14 +0530

    Done

--- Learning/Maths/Combinatorics/basic.cpp
+++ Learning/Maths/Combinatorics/basic.cpp
@@ -2,37 +2,46 @@
 using namespace std;
 typedef long long ll;
 
-const ll MAXN = 1e6;
+const ll MAX_N = 1e6;
 const ll mod = 1e9 + 7;
 
-ll fact[MAXN];
-
+ll fact[MAX_N];
+ll power(ll a, ll b);
+// Using Fermit's Theorem
+ll inverse(ll a) { return power(a, mod - 2); }
+ll modA(ll a, ll b) { return ((a % mod) + (b % mod)) % mod; }
+ll modM(ll a, ll b) { return ((a % mod) * (b % mod)) % mod; }
+ll modS(ll a, ll b) { return ((a % mod) - (b % mod)) % mod; }
+ll modD(ll a, ll b) { return ((a % mod) * (inverse(b) % mod)) % mod; }
 ll power(ll a, ll b) {
-  if (b <= 0) return 1;
-  if (b % 2 == 0)
-    return (power(a, b / 2) * power(a, b / 2)) % mod;
-  else
-    return (((power(a, b / 2) * power(a, b / 2)) % mod) * a) % mod;
+  ll ans = 1, temp = a;
+  while (b) {
+    if (b & 1) ans = modM(ans, temp);
+    b >>= 1;
+    temp = modM(temp, temp);
+  }
+  return ans;
 }
 
-//L-() and how?
-ll inverse(ll a) { return power(a, mod - 2); }
+// L-() and how?
+// ll inverse(ll a) { return power(a, mod - 2); }
 
 void calculate_factorial() {
   fact[0] = 1;
-  for (int i = 1; i < MAXN; i++) {
-    fact[i] = (fact[i - 1] * i) % mod;
+  for (int i = 1; i < MAX_N; i++) {
+    fact[i] = modM(fact[i - 1], i);
   }
 }
 
 ll nCr(ll n, ll r) {
   if (n < 0 || r < 0 || r > n) return 0;
 
-  return ((fact[n]) / (fact[r] * fact[n - r]) % mod) % mod;
+  ll bottom = modM(inverse(fact[r]), inverse(fact[n - r]) % mod);
+  return modM(fact[n], bottom);
 }
 
 int main() {
   calculate_factorial();
-  cout << inverse(10);
+  cout << nCr(10, 5);
   return 0;
 }
