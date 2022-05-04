1e411c30f64d876b27c90c2bb6527c17f7496666
code-shoot <franksmithattheshoot@gmail.com>
2022-05-04 13:37:43 +0530

    always consider and loop through each elements in these type of questions

---
+++ challenges/786 (Div 3)/E.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+
+#define code_brains                 \
+  ;                                 \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(0);
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double lld;
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+#define endl "\n"
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define debug(x)     \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
+#define present(c, x) (c.find(x) != c.end())
+#define desc greater<int>()
+#define fi first
+#define se second
+#define bg begin()
+#define ed end()
+#define set_bits __builtin_popcountint
+#define vl vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+
+int M = 1e9 + 7;
+int power(int a, int n) {
+  int result = 1;
+  while (n) {
+    if (n & 1) result = (result * (ll)a) % M;
+    n >>= 1;
+    a = (a * (ll)a) % M;
+  }
+  return result;
+}
+
+int power(int a, int n);
+int minv(int a) { return power(a, M - 2); }
+int mod(int n) { return (n % M + M) % M; }
+int modM(int n, int m) { return ((ll)(n % M) * (m % M)) % M; }
+int modA(int n, int m) { return ((ll)(n % M) + (m % M)) % M; }
+int modS(int n, int m) { return (((ll)(n % M) - (m % M)) + M) % M; }
+int modD(int n, int m) { return ((ll)(n % M) * (minv(m) % M)) % M; }
+int lcm(int a, int b) { return ((ll)a * b) / __gcd(a, b); }
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.first);
+  cerr << ",";
+  _print(p.second);
+  cerr << "}";
+}
+template <class T>
+void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+///////////////////////////////////////////////////////////////
+
+signed main() {
+  code_brains;
+  ll n;
+  cin >> n;
+  vector<ll> arr(n);
+  for (auto &i : arr) cin >> i;
+
+  ll adj = 1e10, x, y, a = 1e15, b = 1e15, ans1 = 1e15;
+
+  for (int i = 0; i < n - 1; i++) {
+    ll mx = max(arr[i], arr[i + 1]), mn = min(arr[i], arr[i + 1]);
+    
+    if (mx > 2 * mn) {
+      ans1 = min(ans1, (mx + 1) / 2);
+    } else {
+      ans1 = min(ans1, (mx + mn + 2) / 3);
+    }
+
+    if (i < n - 2) {
+      if (arr[i] + arr[i + 2] < a + b) {
+        a = arr[i];
+        b = arr[i + 2];
+      }
+    }
+  }
+
+  sort(all(arr));
+
+  ll biggest = arr[0], big = arr[1];
+
+  ll ans2 = ((biggest + 1) / 2) + (big + 1) / 2;
+
+  // ll ans3 = (max(a, b) - abs(a - b));
+  // ans3 += (abs(a - b) + 1) / 2;
+  ll ans3 = a / 2 + b / 2 + (((a & 1) || (b & 1)) ? 1 : 0);
+  // debug(ans1);
+  // debug(ans2);
+  // debug(ans3);
+
+  cout << min(ans1, min(ans2, ans3)) << '\n';
+
+  return 0;
+}
+///////////////////////////////////////////////////////////////
