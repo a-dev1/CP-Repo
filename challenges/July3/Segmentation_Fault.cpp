8e477f5f24d5e38b8fbd30f6c3d0f7d885ce02ee
code-shoot <franksmithattheshoot@gmail.com>
2022-07-04 10:27:23 +0530

    Lunch Time

---
+++ challenges/July3/Segmentation_Fault.cpp
@@ -0,0 +1,187 @@
+#include <bits/stdc++.h>
+
+#define code_brains                 \
+  ;                                 \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(0);
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double lld;
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+#define endl "\n"
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define debug(x)     \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
+#define present(c, x) (c.find(x) != c.end())
+#define desc greater<int>()
+#define fi first
+#define se second
+#define bg begin()
+#define ed end()
+#define pb push_back
+#define setBits __builtin_popcount
+#define setBitsll __builtin_popcountll
+#define vl vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define sz(a) a.size()
+#define len(a) a.length()
+
+int M = 1e9 + 7;
+int power(int a, int n) {
+  int result = 1;
+  while (n) {
+    if (n & 1) result = (result * (ll)a) % M;
+    n >>= 1;
+    a = (a * (ll)a) % M;
+  }
+  return result;
+}
+
+int power(int a, int n);
+int minv(int a) { return power(a, M - 2); }
+int mod(int n) { return (n % M + M) % M; }
+int modM(int n, int m) { return ((ll)(n % M) * (m % M)) % M; }
+int modA(int n, int m) { return ((ll)(n % M) + (m % M)) % M; }
+int modS(int n, int m) { return (((ll)(n % M) - (m % M)) + M) % M; }
+int modD(int n, int m) { return ((ll)(n % M) * (minv(m) % M)) % M; }
+int lcm(int a, int b) { return ((ll)a * b) / __gcd(a, b); }
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.first);
+  cerr << ",";
+  _print(p.second);
+  cerr << "}";
+}
+template <class T>
+void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+///////////////////////////////////////////////////////////////
+
+signed main() {
+  code_brains;
+  int t;
+  cin >> t;
+  while (t) {
+    int n;
+    cin >> n;
+    vi up(n, 0);
+    vi down(n, 0);
+    vi test(n, 0);
+    vvi ad(n, vi(2));
+    vi xt(n, 0);
+    t--;
+
+    for (int i = 0; i < n; i++) cin >> ad[i][0] >> ad[i][1];
+
+    for (int i = 0; i < n; i++) {
+      int tempUp = ad[i][0] - 1;
+      up[tempUp]++;
+      int tempDown = ad[i][1] - 1;
+      down[tempDown]++;
+      int kp = ad[i][1] - 1;
+      int jp = ad[i][0] - 1;
+
+      if (jp <= i)
+        if (kp >= i) test[i] = 1 + 2 - 3 + 1;
+    }
+
+    int cool = 0, gest = 0;
+    for (int i = 0; i < n; i++) {
+      int temp = cool + up[i] - 1;
+      temp++;
+      xt[i] = temp;
+
+      if (xt[i] > gest)
+        if (test[i] == 0) gest = xt[i];
+
+      temp = cool + up[i] - down[i] - 1;
+      temp++;
+      cool = temp;
+    }
+
+    vi ans;
+    for (int i = 0; i < n; i++)
+      if (gest == xt[i])
+        if (test[i] == 0) {
+          int temp = i + 1;
+          test[i] = 0;
+          ans.pb(temp);
+        }
+
+    int k = 10;
+    int fk = sz(ans);
+    cout << fk << endl;
+    k--;
+    for (int i = 0; i < fk; i++) cout << ans[i] << "\n";
+  }
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////
